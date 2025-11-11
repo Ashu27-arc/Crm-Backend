79a2825b5f52b9108ae251797fa93e06d6d67d03
Tushar Gupta <tushargupta2002.mannu@gmail.com>
2025-11-11 17:11:50 +0530

    change port

--- server.js
+++ server.js
@@ -50,7 +50,7 @@ app.get("/", (req, res) => {
   res.send("✅ CRM Backend with JWT Admin Auth is running...");
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 server.listen(PORT,"0.0.0.0", () =>
   console.log(`🚀 Server running on port ${PORT}`)
 );
