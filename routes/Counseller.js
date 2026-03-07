c192413a049a78ddaaefb8e769fdf5da20864874
Tushar Gupta <tushargupta2002.mannu@gmail.com>
2026-03-07 17:40:20 +0530

    fix error

--- routes/Counseller.js
+++ routes/Counseller.js
@@ -1,9 +1,9 @@
 import express from "express"
-import { BookCounseller, DeleteCounseller, GetCounseller } from "../controller/BookingController.js"
+import { BookCounseller, DeleteCounseller, GetCounseller, GetCounsellerByEmail } from "../controller/BookingController.js"
 import { verifyToken } from "../middleware/auth.js"
 const router = express.Router()
 
-router.get('/booking-details', verifyToken, GetCounseller)
+router.get('/booking-details', GetCounseller)
 router.post('/book', BookCounseller)
 router.post('/delete', DeleteCounseller)
 router.get('/counseller-by-email', GetCounsellerByEmail)
